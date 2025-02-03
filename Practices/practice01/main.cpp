5c47d140116f5bd6b81c926628dd384a45c6c60d
AwAdeleke <awadeleke@ualr.edu>
2025-02-02 21:20:28 -0600

    I made a normal vs project file named practices01 and moved the files from the original practice01 to the new one in vs

--- Practices/practice01/main.cpp
+++ Practices/practice01/main.cpp
@@ -4,7 +4,7 @@
 
 void printByReference(std::vector<int>& numbers) {
     for (int i = 0; i < numbers.size(); i++) {
-       std::cout << numbers.at(i) << std::endl;
+        std::cout << numbers.at(i) << std::endl;
     }
 }
 
@@ -12,46 +12,46 @@ void printByReference(std::vector<int>& numbers) {
 
 int main()
 {
-	std::vector <int> myVector{ 2, 4, 6, 7 };
-     printByReference(myVector);
-   
-   int option{};
-   
-   std::cout 
-   << "Pick a option by inputing the number beside the option\n"
-   << "\tAdd a Number to the Printed Value on above Option 1\n"; 
-   std::cin >> option; 
-   if (option == 1)
-   {
-       std::cout  << "What the number you will like to add\n";
-       int num {};
-       std::cin >> num ;
-       myVector.push_back (num);
-   } 
-   std::cout << "To print the new numbers 1" <<std::endl;
-   int input{};
-   std::cin >> input;
-   if (input ==1){
+    std::vector <int> myVector{ 2, 4, 6, 7 };
     printByReference(myVector);
-   }
-   int sum{0};
-   for (int i = 0; i < myVector.size(); i++) 
-   {
-      sum+=myVector.at(i);
+
+    int option{};
+
+    std::cout
+        << "Pick a option by inputing the number beside the option\n"
+        << "\tAdd a Number to the Printed Value on above Option 1\n";
+    std::cin >> option;
+    if (option == 1)
+    {
+        std::cout << "What the number you will like to add\n";
+        int num{};
+        std::cin >> num;
+        myVector.push_back(num);
+    }
+    std::cout << "To print the new numbers 1" << std::endl;
+    int input{};
+    std::cin >> input;
+    if (input == 1) {
+        printByReference(myVector);
+    }
+    int sum{ 0 };
+    for (int i = 0; i < myVector.size(); i++)
+    {
+        sum += myVector.at(i);
     }
     std::cout << "\nSum of all numbers: " << sum << std::endl;
- 
-    std::cout << "Pick a number to multiple with value in system" <<std::endl;
+
+    std::cout << "Pick a number to multiple with value in system" << std::endl;
     int x{};
-    std::cin >> x ;
-     for (int i =0; i < myVector.size(); i ++)
-     {
-         myVector.at(i)*=x;
-        
-     }
-    
-   printByReference(myVector); 
-    
-    
-    
+    std::cin >> x;
+    for (int i = 0; i < myVector.size(); i++)
+    {
+        myVector.at(i) *= x;
+
+    }
+
+    printByReference(myVector);
+
+
+
 }
